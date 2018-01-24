42f1c0d298fbf88e9aff222395370cf6c12d665d
Bogdan <bogdanmihai@192-168-0-104.rdsnet.ro>
2018-01-24 21:33:39 +0200

    rgee

--- app/services/schedule_service.js
+++ app/services/schedule_service.js
@@ -40,7 +40,7 @@ module.exports = {
                     var table = $('p:first-of-type').html();
                     cheerioTableparser($);
                     console.log(table);
-                    var data = $(table).parsetable();
+                    var data = $(table).parsetable(false, false, true);
                     console.log(data);
                     var started = false;
 
